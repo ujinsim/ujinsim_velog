8d3edd8143c43dea1c260dcfd98fee92b86c50a0
ujinsim <98771235+ujinsim@users.noreply.github.com>
2024-12-17 00:40:07 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -3,7 +3,7 @@ import git
 import os
 
 # 벨로그 RSS 피드 URL
-rss_url = 'https://api.velog.io/rss/@ujinsimss'
+rss_url = 'https://api.velog.io/rss/@ujinsimss_'
 
 # 깃허브 레포지토리 경로
 repo_path = '.'
@@ -21,14 +21,14 @@ repo = git.Repo(repo_path)
 # RSS 피드 파싱
 feed = feedparser.parse(rss_url)
 
-if not feed.entries:
-    print("No entries found in RSS feed.")
-    exit(1)
-
 # 각 글을 파일로 저장하고 커밋
 for entry in feed.entries:
     # 파일 이름에서 유효하지 않은 문자 제거 또는 대체
-    file_name = entry.title.replace('/', '-').replace('\\', '-') + '.md'
+    file_name = entry.title
+    file_name = file_name.replace('/', '-')  # 슬래시를 대시로 대체
+    file_name = file_name.replace('\\', '-')  # 백슬래시를 대시로 대체
+    # 필요에 따라 추가 문자 대체
+    file_name += '.md'
     file_path = os.path.join(posts_dir, file_name)
 
     # 파일이 이미 존재하지 않으면 생성
@@ -40,12 +40,5 @@ for entry in feed.entries:
         repo.git.add(file_path)
         repo.git.commit('-m', f'Add post: {entry.title}\nCo-authored-by: rimgosu <newnyup@gmail.com>')
 
-# 환경 변수에서 PAT 가져오기
-gh_pat = os.environ.get('GH_PAT')
-
-if not gh_pat:
-    print("Error: GH_PAT environment variable not found.")
-    exit(1)
-
 # 변경 사항을 깃허브에 푸시
-repo.git.push(f'https://{gh_pat}@github.com/ujinsim/ujinsim_velog.git')
+repo.git.push()
